b22ac7465ed202162d426a26114078a1db3b00b6
hayoung <iamhayoung6177@gmail.com>
2026-04-17 14:07:28 +0900

    Initial Commit: 첫번째 커밋

---
+++ backend/src/main/java/market_it/pleegie/PleegieApplication.java
@@ -0,0 +1,13 @@
+package market_it.pleegie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PleegieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PleegieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/market_it/pleegie/PleegieApplicationTests.java
@@ -0,0 +1,13 @@
+package market_it.pleegie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PleegieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
